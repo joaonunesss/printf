75321d16102efed158ef86cdb48dfc6a6ca5f8e6
João Nunes <115576458+joaonunesss@users.noreply.github.com>
2023-05-02 15:36:52 +0100

    Add files via upload

--- ft_printf.h
+++ ft_printf.h
@@ -6,16 +6,15 @@
 /*   By: jmarinho <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/27 11:59:53 by jmarinho          #+#    #+#             */
-/*   Updated: 2023/05/02 14:27:19 by jmarinho         ###   ########.fr       */
+/*   Updated: 2023/05/02 15:35:20 by jmarinho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 
-# include <unistd.h>
 # include <stdarg.h>
-# include "libft.h"
+# include "libft/libft.h"
 
 int	ft_printf(const char *format, ...);
 int	ft_print_int(int nbr);
